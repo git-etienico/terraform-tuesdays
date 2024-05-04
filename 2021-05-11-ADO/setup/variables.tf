8a1cf6a2f1d53ec061f271debda94d0f2f936e4a
Nicolas ETIENNE <nicolas.etienne@elgon.lu>
2024-05-04 17:11:05 +0200

    01

--- 2021-05-11-ADO/setup/variables.tf
+++ 2021-05-11-ADO/setup/variables.tf
@@ -1,7 +1,6 @@
 variable "ado_org_service_url" {
   type        = string
   description = "Org service url for Azure DevOps"
-  default     = "https://dev.azure.com/sandboxproject01"
 }
 
 variable "ado_github_repo" {
